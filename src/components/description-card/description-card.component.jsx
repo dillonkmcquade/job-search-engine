5c39d3cb356fd8353ae3e1810406c86db5923dd4
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-25 18:40:45 -0500

    css change

--- src/components/description-card/description-card.component.jsx
+++ src/components/description-card/description-card.component.jsx
@@ -17,6 +17,7 @@ const DescriptionCard = ({
 
       <p>IPSUM Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam veniam quisquam ex obcaecati possimus perferendis quam esse doloribus, consequatur id unde vel placeat culpa voluptatum veritatis incidunt debitis facere exercitationem.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis ducimus non doloribus eligendi veritatis! Suscipit, blanditiis dolorum quos animi voluptatum, ratione sit facere mollitia consequatur unde dicta at alias? Animi.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas culpa officia eveniet ut eum cumque quam excepturi hic. Facilis odit ipsam doloremque debitis repellat repudiandae, culpa aperiam adipisci eveniet est?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque consequatur earum, placeat dolores nam odio ipsam corporis quos itaque aliquam voluptates deserunt harum velit mollitia ducimus expedita numquam delectus rerum.</p>
     </div>
   );
 }
